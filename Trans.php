ecce9d080ec753673dd61a84dd098f00a6985a1c
dongho <backack2@gmail.com>
2022-01-15 05:37:50 +0900

    부모객체의 변수명으로 변경

--- Trans.php
+++ Trans.php
@@ -5,9 +5,9 @@ class Trans {
     public function __construct($charger)
     {
         $charger_properties = get_object_vars($charger);
-        $transb_properties = get_object_vars($this);
+        $trans_properties = get_object_vars($this);
         foreach ($charger_properties as $c_property=>$c_val){
-            foreach($transb_properties as $t_property=>$t_val){
+            foreach($trans_properties as $t_property=>$t_val){
                 if($c_property == $t_property){
                     $this->{$t_property} = $c_val;
                 }
